f41471eef2f32ded1b779fa6130f628803f8db96
Boris <89155393281rb@gmail.com>
2023-11-09 19:48:48 +0300

    второй коммит

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -10,21 +10,19 @@ menu = [{'title': 'Главная', 'url_n': 'women'},
 ]
 
 def index(request):
-    t = render_to_string('women/index.html')
-    return HttpResponse(t)
+    return render(request,'women/index.html', {'menu': menu})
+
 
 def school(request): # HttpRequest
-    t2 = render_to_string('women/school.html')
-    return HttpResponse(t2)
-
+    return render(request,'women/school.html', {'menu': menu})
 def camp(request): # HttpRequest
-    return HttpResponse('это мой лагерь <img height=100% width=100% src= "https://тур63.рф/wp-content/uploads/2019/06/administrator-1.jpg">')
+    return render(request,'women/camp.html', {'menu': menu})
 
 def gym(request): # HttpRequest
-    return HttpResponse('это мой спортивный зал <img src= "https://sportishka.com/uploads/posts/2022-11/1667566300_35-sportishka-com-p-trenazheri-v-trenazhernom-zale-vkontakte-39.jpg">')
+    return render(request,'women/gym.html', {'menu': menu})
 
 def stadium(request): # HttpRequest
-    return HttpResponse('это мой стадион <img src= "http://sd-expert.ru/img/13081947.jpg">')
+    return render(request,'women/stadium.html', {'menu': menu})
 
 def categories(request, cat_id):
     if cat_id == 1:
